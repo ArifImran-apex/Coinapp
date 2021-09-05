b4c5e3d9559e5a28a27a3f347a6d096c1648fcf1
Arif Imran <pk-imran.arif@outlook.com>
2021-09-05 15:10:31 +0500

    fontawesome

--- CoinApp/AssemblyInfo.cs
+++ CoinApp/AssemblyInfo.cs
@@ -1,3 +1,17 @@
-﻿using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("RobotoLight.ttf", Alias = "Roboto")]
+[assembly: ExportFont("Roboto-Medium.ttf", Alias = "Roboto-Medium")]
+[assembly: ExportFont("Roboto-Regular.ttf", Alias = "Roboto-Regular")]
+[assembly: ExportFont("Avenir-Heavy.ttf", Alias = "Avenir-Heavy")]
+[assembly: ExportFont("Avenir-Medium.ttf", Alias = "Avenir-Medium")]
+[assembly: ExportFont("Pacifico.ttf", Alias = "Pacifico")]
+[assembly: ExportFont("SFProDisplayMedium.ttf", Alias = "SFProDisplayMedium")]
+[assembly: ExportFont("SFProDisplayRegular.ttf", Alias = "SFProDisplayRegular")]
+[assembly: ExportFont("SourceSansPro-Bold.ttf", Alias = "SourceSansPro-Bold")]
+
+[assembly: ExportFont("Font Awesome 5 Brands-Regular-400.otf", Alias = "FontAwesomeBrands")]
+[assembly: ExportFont("Font Awesome 5 Free-Regular-400.otf", Alias = "FontAwesomeRegular")]
+[assembly: ExportFont("Font Awesome 5 Free-Solid-900.otf", Alias = "FontAwesomeSolid")]
